ac171f9b69afe7c3eb7b423560a82ce5dcb9518c
PlatonKynin <kininplaton@gmail.com>
2025-03-16 16:34:45 +0300

    HW12 - * 3 - дописать тип и логику функции change + * 4 - передать пропсы в SuperSelect Но не понимаю как подвинуть текст в select-е

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -14,40 +14,43 @@ import {changeThemeId} from './bll/themeReducer'
 * */
 
 const themes = [
-    {id: 1, value: 'light'},
-    {id: 2, value: 'blue'},
-    {id: 3, value: 'dark'},
+   {id: 1, value: 'light'},
+   {id: 2, value: 'blue'},
+   {id: 3, value: 'dark'},
 ]
 
 const HW12 = () => {
-    // взять ид темы из редакса
-    // const themeId = 1
-    const themeId = useSelector((state: AppStoreType ) => state.theme.themeId) // ✅
+   // взять ид темы из редакса
+   // const themeId = 1
+   const themeId = useSelector((state: AppStoreType) => state.theme.themeId) // ✅
+   const dispatch = useDispatch()
 
-    const change = (id: any) => { // дописать функцию
+   const change = (id: number) => { // дописать функцию
+      dispatch(changeThemeId(id))
+   }
 
-    }
+   useEffect(() => {
+      document.documentElement.dataset.theme = themeId + ''
+   }, [themeId])
 
-    useEffect(() => {
-        document.documentElement.dataset.theme = themeId + ''
-    }, [themeId])
-
-    return (
-        <div id={'hw12'}>
-            <div id={'hw12-text'} className={s2.hwTitle}>
-                Homework #12
-            </div>
-
-            <div className={s2.hw}>
-                <SuperSelect
-                    id={'hw12-select-theme'}
-                    className={s.select}
-                    // сделать переключение тем
-
-                />
-            </div>
-        </div>
-    )
+   return (
+      <div id={'hw12'}>
+         <div id={'hw12-text'} className={s2.hwTitle}>
+            Homework #12
+         </div>
+         <div>Выберите тему</div>
+         <div className={s2.hw}>
+            <SuperSelect
+               id={'hw12-select-theme'}
+               className={s.select}
+               // сделать переключение тем
+               options={themes}
+               value={themeId}
+               onChangeOption={change}
+            />
+         </div>
+      </div>
+   )
 }
 
 export default HW12
